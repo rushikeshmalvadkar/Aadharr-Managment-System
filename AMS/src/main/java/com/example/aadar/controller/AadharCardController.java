aa9d2f7438f195f7b8df45eed5aad69f1bb0f531
rushikesh malvadkar <malvadkarrushikesh200@gmail.com>
2022-09-28 23:00:02 +0530

    SHOOT 1 : CREATE AADHARCARDCONTROLLER,GLOBELEXCAPTION HANDLE,AADHARCARDCREATEDTO,AADARCARDRESPONSE DTO AND UPDATEREQUESTDTO

---
+++ AMS/src/main/java/com/example/aadar/controller/AadharCardController.java
@@ -0,0 +1,82 @@
+package com.example.aadar.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.aadar.dto.AadharCardCreateRequestDto;
+import com.example.aadar.dto.AadharCardResponseDto;
+import com.example.aadar.dto.AadharCardUpdateRequestDto;
+import com.example.aadar.response.AadharCardResponseBody;
+import com.example.aadar.service.AadharCardService;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * this class responsible for get the http request
+ * 
+ * @author RUSHIKESH MALVADKAR
+ *
+ */
+@RestController
+@RequestMapping("/api/aadhar")
+@Slf4j
+public class AadharCardController {
+	
+	
+
+
+
+	@Autowired
+	private AadharCardService aadharCardService;
+
+	@PostMapping("/cards")
+	public AadharCardResponseBody<AadharCardCreateRequestDto> createAadharCard(
+			@Valid @RequestBody AadharCardCreateRequestDto createAadharDto) {
+		log.debug("<<<<< createAadharCard()");
+		AadharCardCreateRequestDto createAadharDto1 = this.aadharCardService.createAadharCard(createAadharDto);
+		log.debug("createAadharCard() >>>>>>");
+		return new AadharCardResponseBody<>(createAadharDto1, HttpStatus.CREATED.value());
+
+	}
+
+	@GetMapping("/cards")
+	public AadharCardResponseBody<List<AadharCardResponseDto>> getAllAadharCard() {
+		List<AadharCardResponseDto> allAadharCard = this.aadharCardService.getAllAadharCard();
+		return new AadharCardResponseBody<>(allAadharCard, HttpStatus.OK.value());
+	}
+
+	@GetMapping("/cards/{id}")
+	public AadharCardResponseBody<AadharCardResponseDto> getAadharCardByAadharId(@PathVariable("id") String aadharId) {
+		AadharCardResponseDto aadharCardResponseDto = this.aadharCardService.getAadharCardByAadharId(aadharId);
+
+		return new AadharCardResponseBody<>(aadharCardResponseDto, HttpStatus.OK.value());
+
+	}
+
+	@PutMapping("/cards")
+	public AadharCardResponseBody<AadharCardUpdateRequestDto> updateAadharCard(
+			@Valid @RequestBody AadharCardUpdateRequestDto aadharCard) {
+		AadharCardUpdateRequestDto aadharCardUpdateRequestDto = this.aadharCardService.updateAadharCard(aadharCard);
+		return new AadharCardResponseBody<>(aadharCardUpdateRequestDto, HttpStatus.OK.value());
+	}
+
+	@DeleteMapping("/cards/{id}")
+	public AadharCardResponseBody<Boolean> deleteAadharCardByAadharId(@PathVariable("id") String aadharId) {
+		Boolean isDeleted = this.aadharCardService.deleteAadharCardByAadharId(aadharId);
+		return new AadharCardResponseBody<>(isDeleted, HttpStatus.OK.value());
+	}
+}
---
+++ AMS/src/main/java/com/example/aadar/dto/AadharCardUpdateRequestDto.java
@@ -0,0 +1,67 @@
+package com.example.aadar.dto;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import com.example.aadar.enam.Gender;
+import com.example.aadar.enam.Status;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AadharCardUpdateRequestDto {
+	
+	@NotEmpty(message="aadhar id is not be null or empaty")
+    private String aadharId;
+	
+	@NotEmpty(message="firstname is not be null or empaty")
+    private String firstName;
+	
+	@NotEmpty(message="lastname is not be null or empaty")
+	private String lastName;
+	
+	@NotEmpty(message = "father is not be null or empaty")
+	private String fatherName;
+	
+	@NotNull(message="gender is not be null or empaty")
+	private Gender gender;
+	
+	@NotEmpty(message="phoneNo is not be null or empaty")
+	private String phoneNo;
+	
+	@NotNull(message="status is not be null or empaty")
+	private Status status;
+	
+	@NotNull
+	private LocalDate dateOfBirth;
+	@NotNull
+	private int homeNo;
+	
+	@NotEmpty(message="socity should not be null or empaty")
+	private String socitey;
+	
+	@NotEmpty(message="place should not be null or empaty")
+	private String place;
+	
+	@NotNull
+	private int pincode;
+	
+	@NotEmpty(message="city should not be null or empaty")
+	private String city;
+	
+	@NotEmpty(message="country should not be null or empaty")
+	private String country;
+	
+	@NotNull
+	private LocalDateTime createdOn;
+	
+	@NotNull
+	private Integer addressId;
+	
+	private LocalDateTime addressCreatedOn;
+
+}
